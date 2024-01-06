7902b504a6c992c40368ea65ddba5504314e54f0
Delboy <Derek.degoey@hotmail.co.uk>
2024-01-06 14:40:21 +0000

    Added dummy products

---
+++ frontend/src/products.jsx
@@ -0,0 +1,70 @@
+const products = [
+    {
+        id: '1',
+        name: 'Test Product 1',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'clothes',
+        price: 99.99,
+        hasSize: true,
+        rating: 4.5,
+        reviewCount: 12
+    },
+    {
+        id: '2',
+        name: 'Test Product 2',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'clothes',
+        price: 9.99,
+        hasSize: true,
+        rating: 5,
+        reviewCount: 10
+    },
+    {
+        id: '3',
+        name: 'Test Product 3',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'shoes',
+        price: 49.99,
+        hasSize: true,
+        rating: 3,
+        reviewCount: 12
+    },
+    {
+        id: '4',
+        name: 'Test Product 4',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'shoes',
+        price: 79.99,
+        hasSize: true,
+        rating: 4.5,
+        reviewCount: 12
+    },
+    {
+        id: '5',
+        name: 'Test Product 5',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'beauty',
+        price: 9.99,
+        hasSize: false,
+        rating: 3.5,
+        reviewCount: 5
+    },
+    {
+        id: '6',
+        name: 'Test Product 6',
+        image: "https://picsum.photos/600/600",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        catagory: 'beauty',
+        price: 4.99,
+        hasSize: false,
+        rating: 2,
+        reviewCount: 1
+    },
+]
+
+export default products;
