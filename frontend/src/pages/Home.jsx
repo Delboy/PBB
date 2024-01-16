722827026fbe04d726b48c1a8aadc4ab982a4e67
Delboy <Derek.degoey@hotmail.co.uk>
2024-01-16 13:37:47 +0000

    Added Product3dCarousel component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import HeroCarousel from '../components/HeroCarousel'
 import TestimonialCardSlider from '../components/TestimonialCardSlider'
 import AboutUsBox from '../components/AboutUsBox'
+import Product3dCarousel from '../components/Product3dCarousel'
 
 const Home = () => {
   return (
@@ -10,7 +11,7 @@ const Home = () => {
         <HeroCarousel />
         <TestimonialCardSlider />
         <AboutUsBox />
-        <h1>Product Carousel</h1>
+        <Product3dCarousel />
         <h1>Favourites</h1>
         <h1>Catogories</h1>
         <h1>Simple Testomonies</h1>
