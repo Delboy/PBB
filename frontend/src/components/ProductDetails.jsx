ceed320654c3e07aa3829b6f5011dd27d3d92cab
Delboy <Derek.degoey@hotmail.co.uk>
2024-02-07 13:09:25 +0000

    Added useEffect in productDetails that disables increase qty button if the amount in bag exceeds the amount in stock

--- frontend/src/components/ProductDetails.jsx
+++ frontend/src/components/ProductDetails.jsx
@@ -1,18 +1,44 @@
-import { React, useState } from "react";
-import { addToBag } from '../slices/bagSlice.js'
-import { useDispatch } from 'react-redux'
+import { React, useEffect, useState } from "react";
+import { addToBag } from "../slices/bagSlice.js";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 import Rating from "./Rating";
 
 import classes from "./ProductDetails.module.css";
 
 const ProductDetails = (props) => {
+  const { bagItems } = useSelector((state) => state.bag);
+
   const product = props.product;
 
   const [qty, setQty] = useState(1);
-  const [sizeSelection, setSizeSelection] = useState(product.sizes ? product.sizes[0] : null);
+  const [sizeSelection, setSizeSelection] = useState(
+    product.sizes ? product.sizes[0] : null
+  );
+
+  const [disableIncrease, setDisableIncrease] = useState(false);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const index = bagItems.findIndex(
+      (x) => x._id === product._id && x.size === sizeSelection
+    );
+
+    if (index !== -1) {
+      const qtyInBag = bagItems[index].qty;
+      if (qty + qtyInBag >= product.countInStock) {
+        setDisableIncrease(true);
+      } else {
+        setDisableIncrease(false);
+      }
+    } else if (product.countInStock === qty) {
+      setDisableIncrease(true);
+    } else {
+      setDisableIncrease(false);
+    }
+  }, [bagItems, product._id, product.countInStock, qty, sizeSelection]);
 
   const qtyHandler = (e) => {
     if (e.target.value > 0) {
@@ -27,9 +53,10 @@ const ProductDetails = (props) => {
   };
 
   const addToBagHandler = () => {
-     dispatch(addToBag({...product, qty, size: sizeSelection}))
+    dispatch(addToBag({ ...product, qty, size: sizeSelection }));
+    setQty(1);
     // open cart drawer
-    }
+  };
 
   return (
     <div className={classes.main}>
@@ -80,12 +107,18 @@ const ProductDetails = (props) => {
               -
             </button>
             <p>{qty}</p>
-            <button value={1} onClick={qtyHandler} disabled={qty === product.countInStock}>
+            <button value={1} onClick={qtyHandler} disabled={disableIncrease}>
               +
             </button>
           </div>
 
-          <button className={classes.cartBtn} type='button' onClick={addToBagHandler}>add to cart</button>
+          <button
+            className={classes.cartBtn}
+            type="button"
+            onClick={addToBagHandler}
+          >
+            add to cart
+          </button>
         </div>
       </div>
     </div>
